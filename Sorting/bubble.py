ab2e48a57f444643cd828ea71d004261572a20e6
Jdraiv <josuedr11@gmail.com>
2018-02-25 00:40:54 -0600

    Implemented Bubble Sort

---
+++ Sorting/bubble.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from Tester import Tests
+
+
+def bubble_sort(l):
+    tail = len(l) - 1
+
+    while tail >= 0:
+        for c in range(tail):
+            if l[c + 1] < l[c]:
+                old_val = l[c]
+
+                l[c] = l[c + 1]
+                l[c + 1] = old_val
+        tail -= 1
+    return l
+
+
+test = Tests(bubble_sort)
+test.sort_random_inputs()
+test.show_results()
