88486f72429a86e83c0293cf296d0ec5ea562278
Jdraiv <josuedr11@gmail.com>
2018-02-25 18:25:55 -0600

    Implemented selection sort

---
+++ Sorting/selection.py
@@ -0,0 +1,25 @@
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from Tester import Tests
+
+
+def selection_sort(l):
+    for c, pos in enumerate(l):
+        c_lowest = pos
+
+        for element in range(c, len(l)):
+            if l[element] < c_lowest:
+                c_lowest = l[element]
+
+        l.pop(l.index(c_lowest))
+        l.insert(c, c_lowest)
+    return l
+
+
+test = Tests(selection_sort)
+test.sort_random_inputs()
+test.show_results()
