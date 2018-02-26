d02d9e093e65fa5fd41509cb78aa07e5c2a89eb5
Josue Rivera <josuedr11@gmail.com>
2018-02-25 19:12:14 -0600

    Moved sorting algorithms test class into the sorting directory

--- Sorting/bubble.py
+++ Sorting/bubble.py
@@ -1,10 +1,5 @@
 
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from Tester import Tests
+from SortTest import Tests
 
 
 def bubble_sort(l):
--- Sorting/insertion.py
+++ Sorting/insertion.py
@@ -1,11 +1,5 @@
 
-
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from Tester import Tests
+from SortTest import Tests
 
 
 def insertion_sort(l):
