648710515daf4cd7195f6b772bfaf827e57b7ea6
Jdraiv <josuedr11@gmail.com>
2018-03-04 20:51:55 -0600

    Added runtime information

--- Sorting/test_sort.py
+++ Sorting/test_sort.py
@@ -1,5 +1,6 @@
 
 import random
+import time
 
 
 class Tests(object):
@@ -8,22 +9,34 @@ class Tests(object):
         self.incorrect = 0
         self.algo = algo
 
+    def run_time(self):
+        start_time = time.time()
+        return start_time
+
     def show_results(self):
         print("%s: Passed" % self.correct)
         print("%s: Failed" % self.incorrect)
 
     def sort_custom_input(self, i):
+        t = self.run_time()
+
         if sorted(self.algo(i)):
             self.correct += 1
+            print("Test passed in %s seconds" % (time.time() - t))
         else:
             self.incorrect += 1
+            print("Test failed in %s seconds" % (time.time() - t))
 
     def sort_random_inputs(self):
-        for c in range(random.randint(1, 200)):
+        for c in range(random.randint(1, 50)):
             c = range(0, 11000)
             random_l = random.sample(c, random.randint(1, 200))
 
+            t = self.run_time()
+
             if sorted(self.algo(random_l)):
                 self.correct += 1
+                print("Test passed in: %s seconds" % (time.time() - t))
             else:
                 self.incorrect -= 1
+                print("Test failed in: %s seconds" % (time.time() - t))
